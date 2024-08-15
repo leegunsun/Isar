2635ba325cb4cb06cf556766bf41ec3921f65d0f
leegunsun <talkcjstk@naver.com>
2024-08-15 20:08:35 +0900

    test fin

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,6 +4,8 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -20,44 +22,36 @@ class ApiService {
   Future<void> fetchData(String url) async {
     int retryCount = 0;
 
-    // try {
-    //   final response = await dio.get('https://example.com');
-    // } on DioError catch (e) {
-    //   if (e.type == DioErrorType.other && e.error is SocketException) {
-    //     print('서버에 연결할 수 없습니다. 네트워크 문제일 가능성이 큽니다.');
-    //   } else if (e.type == DioErrorType.connectTimeout) {
-    //     print('서버와의 연결이 타임아웃되었습니다. 서버가 응답하지 않거나 연결이 원활하지 않습니다.');
-    //   } else if (e.type == DioErrorType.receiveTimeout) {
-    //     print('서버 응답이 타임아웃되었습니다. 서버가 응답하지 않거나 연결이 불안정합니다.');
-    //   } else {
-    //     print('다른 오류가 발생했습니다: ${e.message}');
-    //   }
-    // }
-
     while (retryCount < maxRetries) {
       try {
         final response = await dio.get(url);
         print('Success: ${response.data}');
         return; // 성공 시 종료
-      } on DioError catch (e) {
-        if (e.type == DioErrorType.connectTimeout ||
-            e.type == DioErrorType.receiveTimeout) {
-          retryCount++;
-          print('Timeout occurred. Attempt: $retryCount');
+      } catch (e) {
+        if(e is DioError) {
+          if (e.type == DioErrorType.connectTimeout && e.error is SocketException) {
+            print('서버에 연결할 수 없습니다. 네트워크 문제일 가능성이 큽니다.');
+            rethrow;
+          } else if (e.type == DioErrorType.connectTimeout || e.type == DioErrorType.receiveTimeout) {
+            print('서버와의 연결이 타임아웃되었습니다. 서버가 응답하지 않거나 연결이 원활하지 않습니다.');
+            retryCount++;
+            print('Timeout occurred. Attempt: $retryCount');
 
-          if (retryCount == maxRetries) {
-            print('Timeout 5회 이상 발생');
+            if (retryCount == maxRetries) {
+              print('Timeout 5회 이상 발생');
+              rethrow; // 에러를 다시 던짐
+            }
+          } else {
+            print('Error: ${e.message}');
+            rethrow; // 에러를 다시 던짐
           }
         } else {
-          print('Error: ${e.message}');
-          return;
+          rethrow;
         }
       }
     }
   }
 }
-
-
 
 @GenerateMocks([Dio])
 void main() {
@@ -66,19 +60,40 @@ void main() {
     late ApiService apiService;
 
     setUp(() {
-      mockDio = MockDio();  // 이 부분이 제대로 초기화되었는지 확인
+      mockDio = MockDio();
       apiService = ApiService()..dio = mockDio;
     });
 
     test('Should retry up to maxRetries times on timeout', () async {
-      when(mockDio.get(any)).thenAnswer(
-            (_) async => throw DioError(
+      when(mockDio.get(any)).thenThrow(
+        DioError(
+          requestOptions: RequestOptions(path: ''),
+          type: DioErrorType.connectTimeout,
+          error: SocketException('Failed to connect'),
+        ),
+      );
+
+      try {
+        await apiService.fetchData('https://api.example.com/data');
+      } catch (e) {
+        // Do nothing
+      }
+
+      verify(mockDio.get(any)).called(1);
+    });
+
+    test('Should throw error after maxRetries exceeded', () async {
+      when(mockDio.get(any)).thenThrow(
+        DioError(
           requestOptions: RequestOptions(path: ''),
           type: DioErrorType.connectTimeout,
         ),
       );
 
-      await apiService.fetchData('https://api.example.com/data');
+      expect(
+            () async => await apiService.fetchData('https://api.example.com/data'),
+        throwsA(isA<DioError>()),
+      );
 
       verify(mockDio.get(any)).called(apiService.maxRetries);
     });
